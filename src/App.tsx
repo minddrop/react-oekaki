f4bab78c36d04b2a0eb08673f1f4ffcb6c3cc5a0
minddrop <ryooooooo1@gmail.com>
2020-02-25 19:44:48 +0900

    prepared default canvas

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useRef} from 'react'
 
-function App() {
+const App = ({width,height}:{
+  width: number;
+  height: number;
+}) => {
+  const canvasRef = useRef<HTMLCanvasElement>(null);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+    <canvas ref={canvasRef} height={height} width={width} />
+    </>
+  )
 }
+
+App.defaultProps = {
+  width: window.innerWidth,
+  height: window.innerHeight,
+};
 
 export default App;
