a12db77be8ec3ef1c761a8da17bc127e0c816113
minddrop <ryooooooo1@gmail.com>
2020-03-05 12:36:41 +0900

    prettierred

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,35 @@
-import React, {useRef, useState, useEffect, useCallback} from 'react'
+import React, { useCallback, useEffect, useRef, useState } from 'react'
 
 type Coordinate = {
   x: number
   y: number
 }
 
-const App = ({width,height}:{
-  width: number
-  height: number
-}) => {
-  const [isPainting, setIsPainting]= useState(false)
-  const [mousePosition, setMousePosition] = useState<Coordinate | undefined>(undefined)
+const App = ({ width, height }: { width: number; height: number }) => {
+  const [isPainting, setIsPainting] = useState(false)
+  const [mousePosition, setMousePosition] = useState<Coordinate | undefined>(
+    undefined
+  )
 
   const startPaint = useCallback((event: MouseEvent) => {
     const coordinates = getCoordinates(event)
-    if(coordinates){
+    if (coordinates) {
       setIsPainting(true)
       setMousePosition(coordinates)
     }
-  },[])
+  }, [])
 
   const getCoordinates = (event: MouseEvent): Coordinate | undefined => {
-    if(!canvasRef.current) return
-    const canvas:HTMLCanvasElement = canvasRef.current
-    return { x: event.pageX - canvas.offsetLeft, y: event.pageY - canvas.offsetTop }
+    if (!canvasRef.current) return
+    const canvas: HTMLCanvasElement = canvasRef.current
+    return {
+      x: event.pageX - canvas.offsetLeft,
+      y: event.pageY - canvas.offsetTop
+    }
   }
 
   useEffect(() => {
-    if(!canvasRef.current) return
+    if (!canvasRef.current) return
     const canvas: HTMLCanvasElement = canvasRef.current
     canvas.addEventListener('mousedown', startPaint)
     return () => {
@@ -35,21 +37,24 @@ const App = ({width,height}:{
     }
   }, [startPaint])
 
-  const paint = useCallback((event: MouseEvent) => {
-    if(isPainting) {
-      const newMousePosition = getCoordinates(event)
-      if(mousePosition && newMousePosition) {
-        drawLine(mousePosition, newMousePosition)
-        setMousePosition(newMousePosition)
+  const paint = useCallback(
+    (event: MouseEvent) => {
+      if (isPainting) {
+        const newMousePosition = getCoordinates(event)
+        if (mousePosition && newMousePosition) {
+          drawLine(mousePosition, newMousePosition)
+          setMousePosition(newMousePosition)
+        }
       }
-    }
-  }, [isPainting, mousePosition])
+    },
+    [isPainting, mousePosition]
+  )
 
   const drawLine = (from: Coordinate, to: Coordinate) => {
-    if(!canvasRef.current) return
+    if (!canvasRef.current) return
     const canvas: HTMLCanvasElement = canvasRef.current
     const context = canvasRef.current.getContext('2d')
-    if(context) {
+    if (context) {
       context.strokeStyle = 'red'
       context.lineJoin = 'round'
       context.lineWidth = 5
@@ -62,20 +67,20 @@ const App = ({width,height}:{
   }
 
   useEffect(() => {
-    if(!canvasRef.current) return
+    if (!canvasRef.current) return
     const canvas = canvasRef.current
     canvas.addEventListener('mousemove', paint)
     return () => {
       canvas.removeEventListener('mousemove', paint)
     }
-  },  [paint])
+  }, [paint])
 
   const exitPaint = useCallback(() => {
     setIsPainting(false)
   }, [])
 
-  useEffect(()=> {
-    if(!canvasRef.current) return
+  useEffect(() => {
+    if (!canvasRef.current) return
     const canvas = canvasRef.current
     canvas.addEventListener('mouseup', exitPaint)
     canvas.addEventListener('mouseleave', exitPaint)
@@ -85,18 +90,17 @@ const App = ({width,height}:{
     }
   })
 
-
   const canvasRef = useRef<HTMLCanvasElement>(null)
   return (
     <>
-    <canvas ref={canvasRef} height={height} width={width} />
+      <canvas ref={canvasRef} height={height} width={width} />
     </>
   )
 }
 
 App.defaultProps = {
   width: window.innerWidth,
-  height: window.innerHeight,
+  height: window.innerHeight
 }
 
 export default App
